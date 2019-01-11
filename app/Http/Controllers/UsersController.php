b06cc6a11dd175f19bba6cd6ebd9e8a8b0d20987
gavincorleone <gavin_corleone@163.com>
2019-01-11 10:13:49 +0800

    change

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,11 +10,11 @@ class UsersController extends Controller
     //
     public function create()
     {
-        return view('users.create');
+        return view('users/create');
     }
     public function show(User $user)
     {
-        return view('users.show', compact('user'));
+        return view('users/show', compact('user'));
     }
     public function store(Request $request)
     {
